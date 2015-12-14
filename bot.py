c6e88f5a24a54a387098ce6ada83f18af00c9315
pjm <patjm1992@gmail.com>
2015-12-14 18:39:28 -0500

    .

--- bot.py
+++ bot.py
@@ -2,34 +2,46 @@ from irc import *
 
 class Bot:
 
-    def __init__(self, name):
-        irc = IRC()
-        irc.connect("74.75.50.221", "#atildae", name, "bot", "bot", "arch-plex")
+    irc = IRC()
+    
+    def __init__(self, srv, chan, nick, host):
+        self.irc.connect(srv, chan, nick, "bot", "bot", host)
+        self.srv = srv
+        self.chan = chan
+        self.nick = nick
+        self.host = host
 
     def listen_to_server(self):
-        text = irc.get_text()
-        print text
+        text = self.irc.get_text()
+        return text
         
     def say(self, something):
-        irc.PRIVMSG("#atildae", something)
+        self.irc.PRIVMSG("#atildae", something)
 
-    def get_msgs(self):
-        """ Parse text for private messages from users in the channel. """
-        """ TO-DO """
+    def get_msgs(self, text):
+        msg = ""
+        
+        if "PRIVMSG" in text:
+            line = text.split(":")
+            msg = line[2]
+            print ("PRIVMSG RECEIVED: %s" % msg)
+        
         return msg
         
     def change_topic(self, new_topic):
-        irc.TOPIC(new_topic)
+        self.irc.TOPIC(self.chan, new_topic)
         
     def leave(self):
+        pass
         
     def reconnect(self):
-        
+        pass
+    
     def die(self):
         leave()
         sys.exit()
     
     def rename(self, new_name):
-        irc.NICK(new_name)
+        self.irc.NICK(new_name)
 
     
