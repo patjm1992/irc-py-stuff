c547c1a3d03c88b67ee008a058f099e521f571a2
patjm1992 <patjm1992@gmail.com>
2016-10-17 16:52:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ class Bot:
         text = self.irc.get_text()
         return text
         
-    def say(self, something):
-        self.irc.PRIVMSG("#atildae", something)
+    def say(self, chan, something):
+        self.irc.PRIVMSG(chan, something)
 
     def get_msgs(self, text):
         msg = ""
